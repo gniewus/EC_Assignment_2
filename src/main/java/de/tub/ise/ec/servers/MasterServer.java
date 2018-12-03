a61685cdd5ea906ea3f44daef4c1e28e87dcc7aa
Jacek Janczura <janczura.jacek@gmail.com>
2018-12-03 21:37:27 +0100

    Server instances created

---
+++ src/main/java/de/tub/ise/ec/servers/MasterServer.java
@@ -0,0 +1,12 @@
+package de.tub.ise.ec.servers;
+
+/**
+ * Class that extends the SlaveServer.
+ * From this class client can apart from reading from the keystore also write to it.
+ *
+ * This class propagate the changes to it's slaves
+ *
+ * @author Jacek Janczura
+ */
+public class MasterServer extends SlaveServer {
+}
---
+++ src/main/java/de/tub/ise/ec/servers/SlaveServer.java
@@ -0,0 +1,12 @@
+package de.tub.ise.ec.servers;
+
+/**
+ * From this class client can only read the value from the keystore
+ *
+ * This class can propagate the values to other slaves in asynchronus replication
+ *
+ * @author Jacek Janczura
+ */
+public class SlaveServer {
+
+}
